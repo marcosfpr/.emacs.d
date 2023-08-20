c577e466b275b1a4f61cccc0fba60afce1b62a20
marcos <mfpr@air.local>
2023-08-20 09:47:49 -0300

    feat: tab, backtab and dragstuff

--- init.el
+++ init.el
@@ -201,6 +201,10 @@
 ;; Undo/redo layouts
 (winner-mode 1)
 
+(use-package drag-stuff)
+(drag-stuff-define-keys)
+(drag-stuff-global-mode 1)
+
 
 ;; Telephone line
 (use-package telephone-line
@@ -561,6 +565,53 @@
   (setq projectile-switch-project-action 'projectile-dired)
   (setq projectile-git-submodule-command nil)
   :bind (("C-c p f" . projectile-find-file)))
+
+
+(defun indent-region-custom(numSpaces)
+    (progn
+        ; default to start and end of current line
+        (setq regionStart (line-beginning-position))
+        (setq regionEnd (line-end-position))
+
+        ; if there's a selection, use that instead of the current line
+        (when (use-region-p)
+            (setq regionStart (region-beginning))
+            (setq regionEnd (region-end))
+        )
+
+        (save-excursion ; restore the position afterwards
+            (goto-char regionStart) ; go to the start of region
+            (setq start (line-beginning-position)) ; save the start of the line
+            (goto-char regionEnd) ; go to the end of region
+            (setq end (line-end-position)) ; save the end of the line
+
+            (indent-rigidly start end numSpaces) ; indent between start and end
+            (setq deactivate-mark nil) ; restore the selected region
+        )
+    )
+)
+
+(defun untab-region (N)
+    (interactive "p")
+    (indent-region-custom -4)
+)
+
+(defun tab-region (N)
+    (interactive "p")
+    (if (active-minibuffer-window)
+        (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
+    ; else
+    (if (string= (buffer-name) "*shell*")
+        (comint-dynamic-complete) ; in a shell, use tab completion
+    ; else
+    (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
+        (indent-region-custom 4) ; region was selected, call indent-region-custom
+        (insert "    ") ; else insert four spaces as expected
+    )))
+)
+
+(global-set-key (kbd "C-x <backtab>") 'untab-region)
+(global-set-key (kbd "C-x <tab>") 'tab-region)
 
 (server-start)
 
